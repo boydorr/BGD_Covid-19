8765c2c7cbd77d67d493ea18cc9f4231bdf6158f
valiriel <2415275@student.gla.ac.uk>
2021-04-16 15:04:33 +0100

    fixed figure 2 panel spacing

--- B.1.351_resurgence/scripts/2.0_SA_variant_dates_maps_F2.R
+++ B.1.351_resurgence/scripts/2.0_SA_variant_dates_maps_F2.R
@@ -8,7 +8,8 @@ theme <- theme_clean() + theme(axis.title=element_blank(), axis.text=element_bla
                                legend.text=element_text(size=8), legend.title=element_text(size=9), 
                                plot.background = element_rect(color = "white"), 
                                panel.grid = element_blank(), panel.border= element_blank(),
-                               strip.text=element_text(size=10))
+                               strip.text=element_text(size=10), legend.background = element_rect(color = NA),
+                               panel.spacing=unit(0,'npc'))
 
 # import a2i aggregated data
 load(paste0(git.path, "BGD_Covid-19/B.1.351_resurgence/data/data_weekly_a2i.rda"))
@@ -32,7 +33,8 @@ p1 <- ggplot(data=data.weekly, aes(fill = cases)) +
                            breaks = c(1, 10, 100, 1000, data.weekly$cases[which.max(data.weekly$cases)]), 
                            labels = c(1, 10, 100, 1000, data.weekly$cases[which.max(data.weekly$cases)])) +
         facet_wrap(~date, nrow = 1 , ncol = 2) +
-        theme + labs(x = "", y = "", fill="Cases")
+        theme + labs(x = "", y = "", fill="Cases") + 
+        theme(plot.margin = unit(c(0,-2,0,-2), "cm"))
 
 p2 <- ggplot(data=data.weekly, aes(fill = tests)) +
         geom_sf(color="black", size = 0.1) +
@@ -40,7 +42,8 @@ p2 <- ggplot(data=data.weekly, aes(fill = tests)) +
                            breaks = c(1, 10, 100, 1000, 10000, data.weekly$tests[which.max(data.weekly$tests)]), 
                            labels = c(1, 10, 100, 1000, 10000, data.weekly$tests[which.max(data.weekly$tests)])) +
         facet_wrap(~date, nrow = 1 , ncol = 2) +
-        theme + labs(x = "", y = "", fill="Tests")
+        theme + labs(x = "", y = "", fill="Tests") + 
+        theme(plot.margin = unit(c(-2.5,0,0,0), "cm"), strip.text.x = element_blank())
 
 p3 <- ggplot(data=data.weekly, aes(fill = positivity)) +
         geom_sf(color="black", size = 0.1) +
@@ -48,13 +51,14 @@ p3 <- ggplot(data=data.weekly, aes(fill = positivity)) +
                            breaks = seq(0, data.weekly$positivity[which.max(data.weekly$positivity)], by=0.1), 
                            labels = seq(0, data.weekly$positivity[which.max(data.weekly$positivity)], by=0.1)) +
         facet_wrap(~date, nrow = 1 , ncol = 2) +
-        theme + labs(x = "", y = "", fill="Positivity")
+        theme + labs(x = "", y = "", fill="Positivity") + 
+        theme(plot.margin = unit(c(-5.5,0,0,0), "cm"), strip.text.x = element_blank())
 
 panel <- ggpubr::ggarrange(p1 + rremove("y.axis") + rremove("x.axis"), 
                    p2 + rremove("y.axis") + rremove("x.axis"), 
                    p3 + rremove("y.axis") + rremove("x.axis"),
-                   labels = c("A", "B", "C"),
-                   label.x = 0.92, label.y = 0.905,
+                   #labels = c("A", "B", "C"),
+                   #label.x = 0.95, label.y = 0.99,
                    font.label = list(size = 13, color ="grey80"),
                    ncol = 1, nrow = 3)
 
